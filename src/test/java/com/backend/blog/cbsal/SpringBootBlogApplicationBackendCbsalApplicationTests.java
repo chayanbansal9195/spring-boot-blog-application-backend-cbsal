a0a0bd053bdfe31f4830d46eb74fd0ba00f1e086
chayanbansal9195 <chayanbansal1999@gmail.com>
2023-08-05 23:20:21 +0530

    git added initial project setup commit

---
+++ src/test/java/com/backend/blog/cbsal/SpringBootBlogApplicationBackendCbsalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.blog.cbsal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlogApplicationBackendCbsalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
